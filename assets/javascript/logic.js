149c7c65211e1ea700ec4d5fb5da2a0998f88f66
Bob Ross <ministash14@gmail.com>
2018-07-05 17:45:14 -0700

    Adding more functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,24 +1,26 @@
 let user_one_color_choice = 0;
 let page_index = 1;
 let sliderRange = 15;
+let rowNumber = 0;
+let pixelNumber = 0;
 
 
 $(document).ready(function () {
     //Initial things that needs to happen as soon as the document loads
     page_decider(page_index);
-    
+
     function page_decider(page_index) {
         $('.generalWrapper').html('');
         switch (page_index) {
             case 1: page1();
-            break;
+                break;
             case 2: page2();
-            break;
+                break;
             case 3: page3();
-            break;
+                break;
         }
     }
-    
+
     // Start of my page functionality
     function page1() {
         $('.generalWrapper').html('<div id="topMessage">Welcome To Collaborative Art Project!</div><div id="largeButton" class="x">Get Started!</div></div>');
@@ -30,46 +32,51 @@ $(document).ready(function () {
     function page3() {
         $('.generalWrapper').append('<div id="topMessage">Enjoy your project!</div><div id="artBox1"></div></div>');
         $('#sideBar').slideDown(800);
-        
+
     }
-    
+
     function gridSize(sliderRange) {
         console.log('running');
         for (let i = 0; i < sliderRange; i++) {
-            let newDiv = $("<div>");
-            newDiv.attr('class', 'row');
-            $("#artBox1").append(newDiv);
-            
+            rowNumber++;
+            $("#artBox1").append(
+                $("<div>")
+                .attr('class', 'row')
+                .attr('id', rowNumber)
+
+            );
         }
         for (let i = 0; i < sliderRange; i++) {
+            pixelNumber++;
             $('.row').append(
                 $('<div/>')
-                .attr('class', 'pixel')
-            );
-            
+                    .attr('class', 'pixel')
+                    .attr('id', pixelNumber)
+            )
+
         }
-        
+
     }
-    
-    
+
+
     //all my DOM click events
     $(document).on('click', '.pixel', function () {
         user_one_color_choice = $('#colorPicker').val();
-    $(this).css('background', user_one_color_choice);
-});
+        $(this).css('background', user_one_color_choice);
+    });
 
-$(document).on('input', sliderRange, () => {
-    sliderRange = $('#myRange').val();
-    $('#gridInformation').html(sliderRange);
-});
+    $(document).on('input', sliderRange, () => {
+        sliderRange = $('#myRange').val();
+        $('#gridInformation').html(sliderRange);
+    });
 
-$(document).on('click', '#largeButton', function () {
-    sliderRange = $('#myRange').val();
-    page_index++;
-    page_decider(page_index);
-    $('#gridInformation').html(sliderRange);
-    gridSize(sliderRange);
-});
+    $(document).on('click', '#largeButton', function () {
+        sliderRange = $('#myRange').val();
+        page_index++;
+        page_decider(page_index);
+        $('#gridInformation').html(sliderRange);
+        gridSize(sliderRange);
+    });
 
 
 
